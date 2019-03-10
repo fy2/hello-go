6a56c9cf46e0bf6d4b3156d0f2b1f64b1eb4a33a
Feyruz Yalcin <feyruz.yalcin@net-a-porter.com>
2019-03-10 14:22:56 +0000

    use external git repo package

--- hello-go.go
+++ hello-go.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/fy2/stringutil"
+)
+
 
 func main() {
-	fmt.Println("Hello, world.")
+    fmt.Println(stringutil.Reverse("!oG ,olleHFeyruz"))
 }
